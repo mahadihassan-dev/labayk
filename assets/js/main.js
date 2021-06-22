28eaa1ca0103b172d6b273a821edcc25f3203684
Mahedi Hassan <mahadihassan.mhd@gmail.com>
2021-06-23 02:07:20 +0600

    Our Recipies Slide Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,4 +34,40 @@ $('.ProductSlider').slick({
       }
     ]
   });
+
+  // Our Recipies slides
+  $('.recipiesSlider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<img src="./assets/images/arrows/slide-right-arrow.svg" alt="" class="img-fluid ProductsSlideArrow ProductsSlideRightarrow">',
+    prevArrow: '<img src="./assets/images/arrows/slide-left-arrow.svg" alt="" class="img-fluid ProductsSlideArrow ProductsSlideLeftarrow">',
+    responsive: [
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 7,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
